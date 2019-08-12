ee53384e479e20d285c00b05cbf6da862127cc12
Alexander Eberts <alex.eberts@gmail.com>
2019-08-12 16:51:50 -0700

    - Implemented through section 13 - "Log in" - Moved database init to init function (from ^:after-load start function) - Buttons are rendering the wrong color (theme set incorrectly?) - Login Form fields are rendering in the wrong color (theme?)

--- cheffy/CHANGELOG.md
+++ cheffy/CHANGELOG.md
@@ -1,10 +1,11 @@
 # Next: Section 14 Form Group
 
+# 2019-09-12: Section 13 Log In
 
-
-# 2019-07-20: Section 13
-
--
+- Implemented through section 13 - "Log in"
+- Moved database init to init function (from ^:after-load start function)
+- Buttons are rendering the wrong color (theme set incorrectly?)
+- Login Form fields are rendering in the wrong color (theme?)
 
 # 2019-07-16: Section 12
 
--- cheffy/src/app/auth/views/log_in.cljs
+++ cheffy/src/app/auth/views/log_in.cljs
@@ -1,6 +1,38 @@
 (ns app.auth.views.log-in
-  (:require [app.components.page-nav :refer [page-nav]]))
+  (:require [app.components.page-nav :refer [page-nav]]
+            [reagent.core :as r]
+            [re-frame.core :as rf]
+            ["@smooth-ui/core-sc" :refer [Row Col FormGroup Label Input Box Button]]))
 
 (defn log-in
   []
-  [page-nav {:center "Log In"}])
+  (let [initial-values {:email "" :password ""}
+        values (r/atom initial-values)]
+    (fn []
+      [:> Row {:justify-content "center"}
+       [:> Col {:xs 12 :sm 6}
+        [page-nav {:center "Log In"}]
+        [:> FormGroup
+         [:> Label {:html-for :email} "Email"]
+         [:> Input {:control true
+                    :id :email
+                    :type "email"
+                    :value (:email @values)
+                    :on-change #(swap! values assoc :email (.. % -target -value))}]]
+        [:> FormGroup
+         [:> Label {:html-for :password} "Password"]
+         [:> Input {:control true
+                    :id :password
+                    :type "password"
+                    :value (:password @values)
+                    :on-change #(swap! values assoc :password (.. % -target -value))}]]
+        [:> Box {:display "flex"
+                 :justify-content "space-between"}
+          [:> Box {:py 1
+                   :pr 2}
+           [:a {:href "#sign-up"
+                :on-click #(rf/dispatch [:set-active-nav :sign-up])}
+            "New to Cheffy? Create an account!"]]
+          [:> Box
+           [:> Button {:on-click #(js/console.log "log-in")}
+            "Log In"]]]]])))
